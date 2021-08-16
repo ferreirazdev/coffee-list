933c51bfcc6d15f435dbd34b215c8ee0f466e4ef
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-08-16 13:18:00 -0300

    Data fetching with GraphQL and Apollo client

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import CoffeeAutonomous from './components/CoffeeAutonomous';
 import CoffeClick from './components/CoffeeClick';
-import CoffeeInterval from './components/CoffeeInterval'
+import CoffeeInterval from './components/CoffeeInterval';
 import SauceExample from './components/SauceExample';
+import CoffeeGraphQL from './components/CoffeeGraphQL';
 
 export default function App() {
-  return <SauceExample />;
+  return <CoffeeGraphQL />;
 }
 
