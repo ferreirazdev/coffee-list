d93f07c176bfb80e51e3440290e75b1f8843927a
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-08-16 12:50:55 -0300

    Using Apisauce with Hooks

--- App.js
+++ App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import CoffeeAutonomous from './components/CoffeeAutonomous';
 import CoffeClick from './components/CoffeeClick';
 import CoffeeInterval from './components/CoffeeInterval'
+import SauceExample from './components/SauceExample';
 
 export default function App() {
-  return <CoffeeInterval />;
+  return <SauceExample />;
 }
 
