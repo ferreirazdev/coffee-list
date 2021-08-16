0a0ce3080ddcaefd74ee304ea9e45f5c9e606400
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-08-16 10:26:55 -0300

    finish the button fetchData

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import CoffeeAutonomous from './components/CoffeeAutonomous';
+import CoffeClick from './components/CoffeeClick';
 
 export default function App() {
-  return <CoffeeAutonomous />;
+  return <CoffeClick />;
 }
 
